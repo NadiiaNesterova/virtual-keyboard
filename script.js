88bb0089cf48733aaf4926a6b5f31ac24ce83964
Nadiia Nesterova <nesterova1717@gmail.com>
2023-04-28 12:41:42 +0300

    feat: update script.js file

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-const keyboard = document.createElement('div');
-const textarea = document.createElement('textarea');
-
 const engKeys = [
   ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace'],
   ['Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'Del'],
@@ -10,43 +7,123 @@ const engKeys = [
 ];
 
 const rusKeys = [
-  ['Ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace'],
+  ['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace'],
   ['Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '\\', 'Del'],
   ['CapsLock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter'],
   ['Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', '▲', 'Shift'],
   ['Ctrl', 'Alt', ' ', 'Alt', 'Ctrl', '◄', '▼', '►']
 ];
 
+// Создание виртуальной клавиатуры
+const keyboard = document.createElement('div');
+const textarea = document.createElement('textarea');
+
+// Добавление классов для стилизации
 keyboard.classList.add('keyboard');
 textarea.classList.add('textarea');
 
-document.body.append(textarea, keyboard);
+// Переключение между языками клавиатуры по нажатию левых Ctrl и Alt
+let isEng = localStorage.getItem('language') !== 'rus';
+let ctrlPressed = false;
+let altPressed = false;
 
-const generateKeyboard = (keys) => {
-  keyboard.append(
-    ...keys.map((keyRow) => {
-      const row = document.createElement('div');
-      row.classList.add('keyboard-row');
-      row.append(
-        ...keyRow.map((key) => {
-          const keyboardKey = document.createElement('div');
-          keyboardKey.classList.add('keyboard-key');
-          keyboardKey.textContent = key;
-          return keyboardKey;
-        })
-      );
-      return row;
-    })
-  );
-};
+document.addEventListener('keydown', (event) => {
+  if (event.code === 'ControlLeft') ctrlPressed = true;
+  else if (event.code === 'AltLeft') altPressed = true;
 
-let currentLanguage = localStorage.getItem('language') || 'eng';
+  if (ctrlPressed && altPressed) {
+    isEng = !isEng;
+    localStorage.setItem('language', isEng ? 'eng' : 'rus');
+    renderKeys();
+  }
 
-const toggleLanguage = () => {
-  currentLanguage = currentLanguage === 'eng' ? 'rus' : 'eng';
-  localStorage.setItem('language', currentLanguage);
+  if (event.code === 'Tab') {
+    event.preventDefault(); 
+    handleTab(); 
+  }
+});
+
+
+document.addEventListener('keyup', (event) => {
+if (event.code === 'ControlLeft') ctrlPressed = false;
+else if (event.code === 'AltLeft') altPressed = false;
+});
+
+window.addEventListener('load', () => {
+isEng = localStorage.getItem('language') !== 'rus';
+renderKeys();
+});
+
+// Функция для генерации кнопок клавиатуры
+function renderKeys() {
   keyboard.innerHTML = '';
-  currentLanguage === 'eng' ? generateKeyboard(engKeys) : generateKeyboard(rusKeys);
-};
+  const keys = isEng ? engKeys : rusKeys;
 
-generateKeyboard(engKeys);
+  function handleTab() {
+    const textarea = document.querySelector('textarea');
+    const start = textarea.selectionStart;
+    const end = textarea.selectionEnd;
+    const spaces = '    '; // четыре пробела
+    textarea.value = textarea.value.substring(0, start) + spaces + textarea.value.substring(end);
+    textarea.selectionStart = start + spaces.length;
+    textarea.selectionEnd = textarea.selectionStart;
+    textarea.focus();
+  }
+  
+  function handleBackspace() {
+    const textarea = document.querySelector('.textarea');
+    textarea.value = textarea.value.substring(0, textarea.value.length - 1);
+  }  
+
+  function handleSymbol(symbol) {
+    if (symbol === 'Enter') return handleEnter();
+    const lowerSymbol = symbol.toLowerCase();
+    const inputSymbol = isEng ? lowerSymbol : (lowerSymbol === 'ъ' ? 'ь' : lowerSymbol);
+    textarea.value += inputSymbol;
+  }
+  function handleEnter() {
+    textarea.value += '\n';
+  }
+  
+  keys.forEach(row => {
+    const rowElement = document.createElement('div');
+    rowElement.classList.add('row');
+    row.forEach(key => {
+      if (key === 'Backspace') {
+        const keyElement = document.createElement('button');
+        keyElement.classList.add('key');
+        keyElement.textContent = key;
+        keyElement.addEventListener('click', handleBackspace);
+        rowElement.appendChild(keyElement);
+      } else {
+        const keyElement = document.createElement('button');
+        keyElement.classList.add('key');
+        keyElement.textContent = key;
+        if (key === 'Tab') {
+          keyElement.addEventListener('keydown', event => {
+            if (event.code === 'Tab') {
+              event.preventDefault();
+              handleTab();
+            }
+          });
+                  
+          keyElement.addEventListener('click', handleTab);
+        }       
+        else {
+          keyElement.addEventListener('click', () => {
+            const symbol = isEng ? key.toLowerCase() : (key === 'ъ' ? 'ь' : (key === 'Ъ' ? 'Ь' : key));
+            handleSymbol(symbol);
+          });
+        }
+        rowElement.appendChild(keyElement);
+      }
+    });
+    keyboard.appendChild(rowElement);
+  });
+  
+}
+renderKeys();
+
+// Добавление клавиатуры и текстовой области на страницу
+document.body.appendChild(textarea);
+document.body.appendChild(keyboard);
